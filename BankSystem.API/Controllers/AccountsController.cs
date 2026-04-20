e9abfa24ebdc7cad3ec70ac3ff87f7422b3b8573
Cabaya <caccabaya@arcnet.com>
2026-04-20 18:25:44 +0800

    Initial commit - BankSystem API

---
+++ BankSystem.API/Controllers/AccountsController.cs
@@ -0,0 +1,61 @@
+﻿using BankSystem.DTOs;
+using BankSystem.Entities;
+using BankSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IBankAccountService _service;
+
+        public AccountsController(IBankAccountService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("{id}/deposit")]
+        public IActionResult Deposit(int id, [FromBody] TransactionDto dto)
+        {
+            var result = _service.Deposit(id, dto.Amount);
+
+            return HandleResult(result);
+        }
+
+        [HttpPost("{id}/withdraw")]
+        public IActionResult Withdraw(int id, [FromBody] TransactionDto dto)
+        {
+            var result = _service.Withdraw(id, dto.Amount);
+
+            return HandleResult(result);
+        }
+
+        [HttpGet("{id}/balance")]
+        public IActionResult ViewBalance(int id)
+        {
+            var result = _service.ViewBalance(id);
+
+            if (!result.Success)
+                return NotFound(result);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount([FromBody] CreateAccountDto dto)
+        {
+            var created = _service.CreateAccount(dto.Name, dto.AccountType);
+            return Ok(created);
+        }
+
+        private IActionResult HandleResult(ServiceResult result)
+        {
+            if (!result.Success)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ BankSystem.API/Program.cs
@@ -0,0 +1,32 @@
+using BankSystem.Repositories;
+using BankSystem.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// register in services DI Container
+builder.Services.AddSingleton<IAccountRepository, InMemoryAccountRepository>();
+builder.Services.AddScoped<IBankAccountService, BankAccountService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
